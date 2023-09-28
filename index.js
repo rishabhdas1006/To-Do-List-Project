e01f0e303cb13612fa3043564dff6d68b7cf57e7
Prince <prin100603@gmail.com>
2023-09-29 00:14:26 +0530

    Added sign up & checkout pages, redesigned website according to price tiers

--- index.js
+++ index.js
@@ -19,7 +19,17 @@ var headings = [];
 var bodies = [];
 var time = [];
 var done = [];
+var userSignedIn = 0;
+var reenterPassword = 0;
+var username = "Name";
+var useremail = "Email address";
+var userpassword = "";
+var confirmpassword = "";
+const plans = ["TimeTower Basic", "TimeTower Pro", "TimeTower Premium"];
+const planPrices = [0, 4.99, 9.99];
+var plan = 0;
 
+var country_list = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
 
 app.get("/", (req, res) => {
     console.log("New session started");
@@ -27,13 +37,23 @@ app.get("/", (req, res) => {
     bodies = [];
     time = [];
     done = [];
+    userSignedIn = 0;
+    reenterPassword = 0;
+    username = "Name";
+    useremail = "Email address";
+    userpassword = "";
+    confirmpassword = "";
+    plan = 0;
     var page = "home";
     res.render("index.ejs", {
         noteHeadings: headings,
         noteBodies: bodies,
         noteTime: time,
         noteDone: done,
-        notePage: page
+        notePage: page,
+        userSignedUp: userSignedIn,
+        planList: plans,
+        planIndex: plan
     });
 });
 
@@ -45,7 +65,11 @@ app.get("/submit", (req, res) => {
         noteBodies: bodies,
         noteTime: time,
         noteDone: done,
-        notePage: page
+        notePage: page,
+        userSignedUp: userSignedIn,
+        userName: username,
+        planList: plans,
+        planIndex: plan
     });
 });
 
@@ -85,36 +109,126 @@ app.post("/submit", (req, res) => {
     else if(typeof(req.body.marker) === "string"){
         done[(Number)(req.body.marker)] = 1;
     }
-    // console.log(req.body);
-    var page = "home";
     res.redirect("/submit");
 });
 
 app.get("/features", (req, res) => {
     var page = "features";
     res.render("features.ejs", {
-        notePage: page
+        notePage: page,
+        userSignedUp: userSignedIn,
+        userName: username,
+        planList: plans,
+        planIndex: plan
     });
 });
 
 app.get("/pricing", (req, res) => {
     var page = "pricing";
     res.render("pricing.ejs", {
-        notePage: page
+        notePage: page,
+        userSignedUp: userSignedIn,
+        userName: username,
+        planList: plans,
+        planIndex: plan
+    });
+});
+
+app.get("/signup", (req, res) => {
+    var page = "pricing";
+    res.render("signup.ejs", {
+        notePage: page,
+        userSignedUp: userSignedIn,
+        userName: username,
+        mismatchPassword: reenterPassword,
+        enteredName: username,
+        enteredEmail: useremail,
+        planList: plans,
+        planIndex: plan
+    });
+});
+
+app.post("/signup", (req, res)=> {
+    var page = "pricing";
+    userpassword = req.body.password1;
+    confirmpassword = req.body.password2;
+    if(userpassword.localeCompare(confirmpassword) === 0){
+        username = req.body.name;
+        useremail = req.body.email;
+        userSignedIn = 1;
+        reenterPassword = 0;
+        res.redirect("/submit");
+    }
+    else{
+        reenterPassword = 1;
+        res.render("signup.ejs" ,{
+            notePage: page,
+            enteredName: username,
+            userName: username,
+            enteredEmail: useremail,
+            mismatchPassword: reenterPassword,
+            userSignedUp: userSignedIn,
+            planList: plans,
+            planIndex: plan
+        });
+    }
+});
+
+app.post("/checkout", (req, res) => {
+    var page = "pricing";
+    var selectedPlanInd = req.body.plan;
+    if(userSignedIn === 0){
+        res.redirect("/signup");
+    }
+    else{
+        res.render("checkout.ejs", {
+            notePage: page,
+            userSignedUp: userSignedIn,
+            userName: username,
+            userEmail: useremail,
+            planIndex: plan,
+            countryList: country_list,
+            selectedPlanIndex: selectedPlanInd,
+            planList: plans,
+            planPriceList: planPrices
+        });
+    }
+});
+
+app.post("/confirm", (req,res) => {
+    var page = "pricing";
+    console.log(req.body);
+    plan = req.body.plan - '0';
+    console.log(plan);
+    res.render("confirm.ejs", {
+        notePage: page,
+        userSignedUp: userSignedIn,
+        userName: username,
+        planIndex: plan,
+        planList: plans,
+        planPriceList: planPrices
     });
 });
 
 app.get("/faq", (req, res) => {
     var page = "faq";
     res.render("faq.ejs", {
-        notePage: page
+        notePage: page,
+        userSignedUp: userSignedIn,
+        userName: username,
+        planList: plans,
+        planIndex: plan
     });
 });
 
 app.get("/about", (req, res) => {
     var page = "about";
     res.render("about.ejs", {
-        notePage: page
+        notePage: page,
+        userSignedUp: userSignedIn,
+        userName: username,
+        planList: plans,
+        planIndex: plan
     });
 });
 
